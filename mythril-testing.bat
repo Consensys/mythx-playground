f86bc0707f1bffecc51313f40199d0fb4a4b1ea5
Bernhard Mueller <bernhard.mueller11@gmail.com>
2019-02-03 21:25:48 +0700

    Remove unneeded files?

--- mythril-testing.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-SETX MYTHRIL_API_KEY "ace9b42d-4c97-407c-b197-a9b2e5ab6759"
-SETX MYTHRIL_API_URL "https://api.mythril.ai"
-SETX EMAIL "me@example.com"
-echo "Congratulations! You are now set up to use MythOS."
